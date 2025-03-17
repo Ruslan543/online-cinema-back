3560506862ca7fa7340993681bcc779aa235116f
Ruslan543 <rusel45612@gmail.com>
2025-03-17 12:53:02 +0300

    update

--- src/config/telegram.config.ts
+++ src/config/telegram.config.ts
@@ -2,5 +2,5 @@ import { Telegram } from "src/telegram/telegram.interface";
 
 export const getTelegramConfig = (): Telegram => ({
   chatId: "",
-  token: "8028890143:AAELR95dAmkaMBBT2-1jBl-mBfgIYtgX60E",
+  token: process.env.TELEGRAM_TOKEN ?? "",
 });
